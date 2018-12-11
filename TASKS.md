bc352988095209fd52a4cdf5e37876ccd92a5e82
Tomáš Konrády <tomaskonrady@gmail.com>
2018-12-11 21:09:16 +0100

    tasks update

--- TASKS.md
+++ TASKS.md
@@ -1,4 +1,7 @@
 ## Zadání
+- Napiš uprav komponentu App tak, aby procházeli testy.
 
 
 ## Tipy
+
+- [shallow](https://airbnb.io/enzyme/docs/api/shallow.html)
--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -3,8 +3,25 @@ import { shallow } from 'enzyme';
 import App from './App';
 
 describe('App', () => {
-	it('renders correctly', () => {
-		const wrapper = shallow(<App />);
-		console.log(wrapper.debug());
+	let wrapper;
+
+	beforeEach(() => {
+		wrapper = shallow(<App />);
+	});
+
+	it('contains p with text "Hello world"', () => {
+		expect(wrapper.find('p').text()).toEqual('Hello world');
+	});
+
+	it('contains one .App', () => {
+		expect(wrapper.find('.App').exists()).toBe(true);
+	});
+
+	it('contains ul with 2 children', () => {
+		expect(wrapper.find('ul').children().length).toBe(2);
+	});
+
+	it('contains section with className .Section', () => {
+		expect(wrapper.find('section').hasClass('Section')).toBe(true);
 	});
 });
