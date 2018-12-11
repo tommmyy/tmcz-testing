7aa639d903420a9bdb1561789e1062a4d66115bb
Tomáš Konrády <tomaskonrady@gmail.com>
2018-12-11 23:35:50 +0100

    update App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,6 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 import { ThemeProvider, createGlobalStyle } from 'styled-components';
-import { Button } from 'rebass';
+import { Button, Box } from 'rebass';
 import theme from './theme';
 
 const GlobalStyle = createGlobalStyle({
@@ -8,27 +8,34 @@ const GlobalStyle = createGlobalStyle({
 		margin: 0,
 		padding: 0,
 	},
+	input: { padding: '8px' },
 });
 
-const App = () => (
-	<ThemeProvider theme={theme}>
-		<Fragment>
-			<GlobalStyle />
-			<div className="App">
-				<section className="Section">
-					<p>Hello world</p>
-					<ul>
-						<li>first</li>
-						<li>second</li>
-					</ul>
-					<input name="age" />
-					<Button active variant="primary">
+class App extends Component {
+	state = { age: '', clicked: false };
+
+	handleAgeChange = (e) => this.setState({ age: e.target.value });
+	handleClick = (e) => this.setState({ clicked: true });
+
+	render() {
+		const { age, clicked } = this.state;
+
+		return (
+			<ThemeProvider theme={theme}>
+				<Fragment>
+					<GlobalStyle />
+					<p className="Age">{age}</p>
+					<p className="Clicked">{clicked ? 'Clicked!' : ''}</p>
+					<Box>
+						<input name="age" value={age} onChange={this.handleAgeChange} />
+					</Box>
+					<Button active onClick={this.handleClick} variant="primary">
 						Click
 					</Button>
-				</section>
-			</div>
-		</Fragment>
-	</ThemeProvider>
-);
+				</Fragment>
+			</ThemeProvider>
+		);
+	}
+}
 
 export default App;
